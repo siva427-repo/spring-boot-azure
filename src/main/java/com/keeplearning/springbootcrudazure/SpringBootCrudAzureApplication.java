92db2f700f629dc5356276647dbf3d3f2a57cd56
dsivaia1 <sivaiah_dara@optum.com>
2020-03-06 19:02:37 +0530

    initial commit

---
+++ src/main/java/com/keeplearning/springbootcrudazure/SpringBootCrudAzureApplication.java
@@ -0,0 +1,13 @@
+package com.keeplearning.springbootcrudazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudAzureApplication.class, args);
+	}
+
+}
